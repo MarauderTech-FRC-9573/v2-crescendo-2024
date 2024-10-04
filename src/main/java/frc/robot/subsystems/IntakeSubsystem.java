10261091591a77752e448169c3a6ec8a2346b297
Andy Ly <97271338+rake6682@users.noreply.github.com>
2024-10-03 17:24:32 -0700

    started commands for intake

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,26 +4,41 @@ import static frc.robot.Constants.IntakeConstants.*;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel;
+
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase { 
     final CANSparkMax intakeMotor;
     public IntakeSubsystem() {
         intakeMotor = new CANSparkMax(0,    CANSparkLowLevel.MotorType.kBrushed);
-
+        
     }
-
-    public void intake() {
-        intakeMotor.set(intakingSpeed);
+    
+    public Command intake() {
+        return this.startEnd (
+        () -> {
+            intakeMotor.set(intakingSpeed);
+            wait(1000);
+        }, 
+        () -> {
+            stop();
+        });
     }
-
-    public void eject() {
-        intakeMotor.set(ejectingSpeed);
+    
+    public Command eject() {
+        return this.startEnd (
+        () -> {
+            intakeMotor.set(ejectingSpeed);
+        },
+        () -> {
+            stop();
+        });
     }
-
+    
     public void stop() {
         intakeMotor.set(stoppingSpeed);
     }
-
-
+    
+    
 }
