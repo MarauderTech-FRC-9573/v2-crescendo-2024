aa4ae413960b15be5eeab74e5d77d4eb17669c75
9573maraudertech <162756411+9573maraudertech@users.noreply.github.com>
2024-10-11 17:38:16 -0700

    use beam break sensor to prevent the note from potentially shooting
    Co-Authored-By: Julian Garcia <69781917+julianalg@users.noreply.github.com>

--- src/main/java/frc/robot/commands/AutoMoveIntake.java
+++ src/main/java/frc/robot/commands/AutoMoveIntake.java
@@ -5,19 +5,26 @@ import static frc.robot.Constants.IntakeConstants.intakingSpeed;
 import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DigitalGlitchFilter;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 
 public class AutoMoveIntake extends Command {
     private DriveSubsystem driveSubsystem;
     private IntakeSubsystem intakeSubsystem;
+    private ShooterSubsystem shooterSubsystem;
     CANSparkMax intakeMotor;
+    DigitalInput beamBreaker;
     
-    public AutoMoveIntake(DriveSubsystem driveSubsystem, IntakeSubsystem intakeSubsystem) {
+    public AutoMoveIntake(DriveSubsystem driveSubsystem, IntakeSubsystem intakeSubsystem, ShooterSubsystem shooterSubsystem) {
         this.driveSubsystem = driveSubsystem;
         this.intakeSubsystem = intakeSubsystem;
+        this.shooterSubsystem = shooterSubsystem;
         this.intakeMotor = intakeSubsystem.intakeMotor;
+        this.beamBreaker = shooterSubsystem.beamBreaker;
 
     }
     
@@ -29,7 +36,11 @@ public class AutoMoveIntake extends Command {
     @Override
     public void execute() {
         driveSubsystem.driveArcade(0.5, 0);
-        intakeMotor.set(intakingSpeed);
+        if (beamBreaker.get()) {
+            intakeMotor.set(intakingSpeed);
+        } else {
+            intakeMotor.set(0);
+        }
         
     }
 }
