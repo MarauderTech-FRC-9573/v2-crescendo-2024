0af004cf28c8544415756bd3c4f47883fd0da220
Julian Garcia <69781917+julianalg@users.noreply.github.com>
2024-11-02 10:12:58 -0700

    Update IntakeSubsystem.java

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -9,44 +9,49 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 public class IntakeSubsystem extends SubsystemBase { 
-    public final CANSparkMax intakeMotor;
-    public final DigitalInput beamBreaker;
-    public IntakeSubsystem() {
-        intakeMotor = new CANSparkMax(6, CANSparkLowLevel.MotorType.kBrushed);
-        intakeMotor.setSmartCurrentLimit(kIntakeCurrentLimit);
-        this.beamBreaker = new DigitalInput(0);
-        
-    }
-
-    public void stop() {
-        intakeMotor.set(stoppingSpeed);
-    }
-
-    public DigitalInput getBeamBreak() {
-        return beamBreaker;
-    }
-
-    public void noteinPlace(CommandXboxController xboxController1, CommandXboxController xboxController2) {
-      GenericHID controller1 = xboxController1.getHID();
-      GenericHID controller2 = xboxController2.getHID();
+  public final CANSparkMax intakeMotor;
+  public final DigitalInput beamBreaker;
+  public IntakeSubsystem() {
+    intakeMotor = new CANSparkMax(6, CANSparkLowLevel.MotorType.kBrushed);
+    intakeMotor.setSmartCurrentLimit(kIntakeCurrentLimit);
+    this.beamBreaker = new DigitalInput(0);
+    
+  }
+  
+  public void stop() {
+    intakeMotor.set(stoppingSpeed);
+  }
+  
+  public DigitalInput getBeamBreak() {
+    return beamBreaker;
+  }
+  
+  public void noteinPlace(CommandXboxController xboxController1, CommandXboxController xboxController2) {
+    GenericHID controller1 = xboxController1.getHID();
+    GenericHID controller2 = xboxController2.getHID();
     if (!beamBreaker.get()) {
-      controller1.setRumble(RumbleType.kBothRumble, 1.0);
-
-      controller2.setRumble(RumbleType.kBothRumble, 1.0);
+      while (!beamBreaker.get()) {
+        controller1.setRumble(RumbleType.kBothRumble, 0.5);
+        controller2.setRumble(RumbleType.kBothRumble, 0.5);
+        new WaitCommand(1);
+        controller1.setRumble(RumbleType.kBothRumble, 0);
+        controller2.setRumble(RumbleType.kBothRumble, 0);      
+      }
     } else {
       controller1.setRumble(RumbleType.kBothRumble, 0);
       controller2.setRumble(RumbleType.kBothRumble, 0);
     }
+  }
+  
+  public void turnON() {
+    if (!beamBreaker.get()) {
+      intakeMotor.set(0);
+    } else {
+      intakeMotor.set(intakingSpeed);
     }
-    
-    public void turnON() {
-        if (!beamBreaker.get()) {
-          intakeMotor.set(0);
-      } else {
-          intakeMotor.set(intakingSpeed);
-      }
-    }
+  }
 }
